7876da74c4ede84f6a689bc339fffd15e450456e
John Crosby <john.crosby@mlb.com>
2021-05-26 16:50:37 -0400

    chore: init

---
+++ README.md
@@ -0,0 +1,6 @@
+To start this project:
+
+1. `yarn install`
+2. `yarn start`
+
+This will start the project. Please start development with the Teams.js component which has the question at the top.
---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Teams from '../components/Teams';
+
+
+const App = () => {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <Teams/>
+          </header>
+        </div>
+      );
+};
+
+export default App;
