2ad88651f46e5e93c6b050bbc35e2e532778f580
John Crosby <john@crsby.com>
2021-09-20 10:21:06 -0400

    fixed issue with initial HTML layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import Teams from '../components/Teams';
-
+import React from "react";
+import Teams from "../components/Teams";
 
 const App = () => {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <Teams/>
-          </header>
-        </div>
-      );
+  return (
+    <div className="App">
+      <header className="App-header" />
+      <Teams />
+    </div>
+  );
 };
 
 export default App;
